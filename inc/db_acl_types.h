7ab281c5a5416611ba2e648e8d32aaa23629e4d8
Kannan Narayanan <kannan_narayanan@dell.com>
2014-08-22 15:55:17 -0700

    First commit

--- inc/db_acl_types.h
+++ inc/db_acl_types.h
@@ -4,6 +4,30 @@
 #include "db_acl_qualifiers.h"
 #include "db_acl_actions.h"
 
+#define ACL_ALL_PORTS 0xffff
 
+typedef uint16_t port_no;
+typedef struct {
+    db_acl_qualifierinfo qual_enum;
+    db_acl_qualifier_mask_t acl_qualmask;
+} db_acl_qualmask_detail_t;
+
+typedef struct {
+    db_acl_actioninfo action_enum;
+    db_acl_action_t action;
+} db_acl_action_detail_t;
+
+typedef struct {
+    db_acl_port_type_t  port_type;
+    db_acl_qualifier_mask_t acl_qualmask;
+} db_acl_action_detail_t;
+
+typedef struct {
+    int db_acl_num_qualifiers;
+    int db_acl_num_actions;
+    int db_acl_num_ports;
+    int db_acl_entry_index;
+    int db_acl_entry_virtual_index;
+} db_acl_entry_metadata_t; 
 
 #endif
