52af12633b9db04679d789635ca8ef4a36534a5c
Mani B <mb@netlogin-sjc-02.force10networks.com>
2014-08-22 13:31:23 -0700

    ACL DB union generation script added

--- bin/mk_acl_union2.sh
+++ bin/mk_acl_union2.sh
@@ -4,11 +4,15 @@ filen=$1
 target_file=$2
 union_name=db_acl_qualifier_mask_t
 
-temp_union_fields=$(cat ../inc/db_acl_qualifier_masks.h  | grep \\\} | tr '}' ' '  | tr ';' ' '  | cut -f2 -d ' ')
+union_fields1=$(cat ../inc/db_acl_qualifier_masks.h  | grep \\\} | tr '}' ' '  | tr ';' ' '  | cut -f2 -d ' ')
 
-union_fields=$(cat ../inc/db_acl_qualifier_masks.h | grep \\mask_t\; | tr '}' ' ' | sed 's/typedef//' | sed -n -e 's/^.*valmask*._t//p' | tr ';' ' ' )
+union_fields2=$(cat ../inc/db_acl_qualifier_masks.h | grep \\mask_t\; | tr '}' ' ' | sed 's/typedef//' | sed -n -e 's/^.*valmask_t//p' | tr ';' ' ' )
 
-real_union_fields=$temp_union_fields$union_fields
+union_fields3=$(cat ../inc/db_acl_qualifier_masks.h | grep \\mask_t\; | tr '}' ' ' | sed 's/typedef//' | sed -n -e 's/^.*valmask[0-9]_t//p' | tr ';' ' ' )
+
+union_fields4=$(cat ../inc/db_acl_qualifier_masks.h | grep \\mask_t\; | tr '}' ' ' | sed 's/typedef//' | sed -n -e 's/^.*valmask[0-9][0-9]_t//p' | tr ';' ' ' )
+
+real_union_fields=$union_fields1$union_fields2$union_fields3$union_fields4
 
 echo "
 #ifndef __${target_file}__
