d05f01c5831f4f6142ad46d7f1dc573a091f19cd
Clifford Wichmann <clifford_wichmann@dell.com>
2014-09-29 14:03:31 -0700

    adding missing doxygen comments

--- inc/ds_event_channel.h
+++ inc/ds_event_channel.h
@@ -13,6 +13,14 @@
 
 #include "std_error_codes.h"
 
+/**
+ * This API initializes the DB event sub system.  This must be done before anyone
+ * tries to use the DS event service and must only be done by one process in AR.
+ *
+ * @return will return STD_ERR_OK on successful execution or a specific return code
+ *        based on the failure.
+ *
+ */
 t_std_error db_event_channel_init(void);
 
 #endif /* DB_EVENT_CHANNEL_H_ */
