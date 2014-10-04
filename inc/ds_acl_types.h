ed624f9ea24254f4c97d69e49ca0e1a12e42393d
Kannan Narayanan <kannan_narayanan@dell.com>
2014-10-03 21:12:06 -0700

    ds_api for ACL

--- inc/ds_acl_types.h
+++ inc/ds_acl_types.h
@@ -23,17 +23,27 @@
 #define ACL_FEATURE_NAME_LEN_MAX 30
 #define ACL_ROOT_PATH  "/etc/dn/acl"
 
+/*
+ * Struct holding ACL Stage 
+ */
 typedef enum {
     ds_acl_STAGE_INGRESS=0,
     ds_acl_STAGE_EGRESS,
     ds_acl_STAGE_PRE_INGRESS,
 } ds_acl_stage_t;
 
+/*
+ * Struct holding ACL Entry Flags
+ */
 typedef enum {
     ds_acl_DELETE_ENTRY = 1,
     ds_acl_ADD_STATS = 2,
 } ds_acl_entry_flags_t;
 
+/*
+ * Struct holding ACL Feature info
+ * 
+ */
 typedef struct  {
     unsigned int feature_id;
     size_t size;
@@ -48,6 +58,10 @@ typedef struct {
     ds_acl_qualifier_mask_t acl_qualmask;
 } ds_acl_qualmask_detail_t;
 
+/*
+* qual_enum identifies the qualifier
+* acl_qualmask is union of all possible qualifiers
+*/
 typedef struct
 {
     ds_acl_action_enum_t action_enum;
@@ -62,13 +76,13 @@ typedef struct
 typedef struct {
     ds_acl_stage_t acl_stage;
     unsigned int num_qualifiers;
-    ds_acl_qualmask_detail_t *qual_array;
+    ds_acl_qualmask_detail_t *qual_array; /* array of num_qualifiers elements */
     unsigned int num_actions;
-    ds_acl_action_detail_t *action_array;
+    ds_acl_action_detail_t *action_array; /* array of num_actions elements */
     port_t num_ports;
-    port_t *port_array;
+    port_t *port_array; /* array of num_ports elements */
     unsigned int entry_index;
-    unsigned int entry_virtual_index;
+    unsigned int entry_virtual_index; /* priority of entry, if different from index */
     ds_acl_entry_flags_t  entry_flags;
 } ds_acl_entry_metadata_t;
 
