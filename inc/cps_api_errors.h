dd7afd9dd1e0a515a6a94142e0e1f518f073a839
Clifford Wichmann <clifford_wichmann@dell.com>
2015-09-11 09:57:36 -0700

    Updated operation rollback to start from last successful commit to the beginning (reverse order of commit)

--- inc/cps_api_errors.h
+++ inc/cps_api_errors.h
@@ -13,10 +13,22 @@
 @{
 */
 
+/**
+ * The CPS allows a return code to be application defined for any given CPS requests like cps_api_get or cps_api_commit.
+ *
+ * The CPS itself also has its own return codes that can be reused by an application if it wishes.  Otherwise
+ * the application can ensure that they return 0 for success otherwise an error will be assumed.
+ *
+ * When an application return a non-zero return code from a CPS callback, the CPS will expect that an error occurs
+ * and will return that result to the client.  If the error occurs during a transaction, the CPS will begin requesting
+ * the rolling back of the transaction.
+ */
+typedef int cps_api_return_code_t;
+
 typedef enum {
     cps_api_ret_code_OK=0, //!< an OK return code
     cps_api_ret_code_ERR=1,//!< a generic error return code
-} cps_api_return_code_t;
+} cps_api_return_code_enum_val_t;
 
 /**
  * @}
