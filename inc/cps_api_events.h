d2e9ca17dc3fe2215812e11e3f0a5f4b4233d1f6
Mike Lazar <mihai_lazar@dell.com>
2016-03-21 17:59:39 -0700

    CPS documentation C and Python API Reference
       Doxygen generated Python and C API documents
       Build CPS API zip package

--- inc/cps_api_events.h
+++ inc/cps_api_events.h
@@ -84,14 +84,14 @@ typedef struct {
 cps_api_return_code_t cps_api_event_service_init(void);
 
 /**
- * @brief initialize to the CPS event forwarding service for to handle events.
- * @param handle the handle that is returned
+ * @brief Connect to the CPS event forwarding service.
+ * @param handle service event handle (returned by the function call)
  * @return standard return code
  */
 cps_api_return_code_t cps_api_event_client_connect(cps_api_event_service_handle_t * handle);
 
 /**
- * Deregister with the event service and therefore remove any registration requests
+ * Disconnect from the event service and therefore remove any registration requests
  * destined for the handle
  *
  * @param handle the handle to the event service
@@ -100,10 +100,11 @@ cps_api_return_code_t cps_api_event_client_connect(cps_api_event_service_handle_
 cps_api_return_code_t cps_api_event_client_disconnect(cps_api_event_service_handle_t handle);
 
 /**
- * Have the client register with the event service for one or more specific events
- * @param handle is the handle to the cps api events service
+ * Register the client with the event service for one or more specific events.
+ * @param handle is the handle to the CPS API events service
  * @param req the registration request that contains all objects that the client wants to register for
  * @return cps_api_ret_code_OK if success otherwise a failure
+ * @sa cps_api_event_client_connect
  */
 cps_api_return_code_t cps_api_event_client_register(cps_api_event_service_handle_t handle,
         cps_api_event_reg_t * req);
@@ -113,32 +114,36 @@ cps_api_return_code_t cps_api_event_client_register(cps_api_event_service_handle
  * @param handle the handle to the CPS event service
  * @param object the object to send
  * @return standard return code
+ * @sa cps_api_event_client_connect
  */
 cps_api_return_code_t cps_api_event_publish(cps_api_event_service_handle_t handle,
         cps_api_object_t object);
 
 /**
- * wait for a object from the event service
+ * Wait for an object from the event service
  * @param handle opened from a previous client registration
  * @param object the to object to receive.
  * @return cps_api_ret_code_OK if the wait was completed with an event returned
  *   or a specific return code indicating a failure or retry request is required
+ * @sa cps_api_event_client_connect
  */
 cps_api_return_code_t cps_api_wait_for_event(cps_api_event_service_handle_t handle,
         cps_api_object_t object);
 
 /** @addtogroup EventThread Event Thread Utilities
- *  This file consists of the APIs to publish and subscribe to events.
+ *  This file provides APIs used to publish and subscribe to events.
  *
  * The following APIs provides a wrapper over the event functionality and will create a
  * thread to receive events from the event service.  There can only be a single one of
- * these in a process.
+ * CPS event thread in a process.
  *
- * The Event Thread will wait for events (with a maximum size) and call the registered
- * callbacks.  There is only one thread so the events will be processed in order.
+ * The Event Thread waits for events (with a maximum size) and calls the registered
+ * callbacks.  All events are processed in the order of their reception.
  *
- * Also remember since it is single threaded the time of the callback will delay the
- * processing of other events that are waiting to be received.
+ * @warning. Since all callback functions are executed in the context of a single thread,
+ * the execution duration of any callback function must be short.
+ * A long execution duration of any callback function will delay the processing of
+ * other events waiting to be received.
  *
  * @{
  * */
@@ -156,30 +161,29 @@ cps_api_return_code_t cps_api_wait_for_event(cps_api_event_service_handle_t hand
 typedef bool (*cps_api_event_thread_callback_t)(cps_api_object_t object,void * context);
 
 /**
- * Initialize the event thread utility.  This will connect to the CPS event
- * service and provide an internal global handle that applications can use
- * to register against events
+ * Initialize the event thread utility.  Connect to the CPS event
+ * service.
  *
- * @return cps_api_ret_code_OK if successful and created otherwise an error.
- *     should only be called once in a process
+ * @note  A process should call this function only once.
+ *
+ * @return cps_api_ret_code_OK if successful, otherwise an error.
  */
 cps_api_return_code_t cps_api_event_thread_init(void);
 
 /**
  * Register for an event and a callback function that will be executed when
- * an event arrives
- * @param reg is the structure that contains the keys and priority of the event
+ * an event arrives.
+ * @param reg the structure that contains the keys and priority of the event
  *     registration
- * @param cb the callback function
- * @param context the context passed to the callback
- * @return cps_api_ret_code_OK if successful
+ * @param cb callback function
+ * @param context context passed to the callback
+ * @return cps_api_ret_code_OK if successful, error code otherwise
  */
 cps_api_return_code_t cps_api_event_thread_reg(cps_api_event_reg_t * reg,
         cps_api_event_thread_callback_t cb, void * context );
 
 /**
- * Publish an event to the CPS API event service using the event thread's
- * global handle.
+ * Publish an event to the CPS API event service.
  *
  * @param object the object to publish
  * @return cps_api_ret_code_OK if successful
