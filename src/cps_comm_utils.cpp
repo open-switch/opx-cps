10686112accee76bea89644c41a701389708a1d4
Cliff Wichmann <clifford_wichmann@dell.com>
2016-04-19 23:04:10 -0700

    Fixing license

--- src/cps_comm_utils.cpp
+++ src/cps_comm_utils.cpp
@@ -1,11 +1,18 @@
 /*
- * cps_comm_utils.cpp
+ * Copyright (c) 2016 Dell Inc.
  *
- *  Created on: Mar 26, 2016
- *      Author: cwichmann
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *  LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ * FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
  */
-
-
 
 #include "cps_comm_utils.h"
 #include "cps_string_utils.h"
@@ -22,12 +29,12 @@
 namespace cps_comms {
 
 std::string cps_generate_unique_process_key() {
-	static std::mutex __lock;
-	std::lock_guard<std::mutex> lg(__lock);
-	static size_t __i =0;
-	pid_t __pid = getpid();
-	pid_t __tid = (pid_t) syscall (SYS_gettid);
-	return cps_string::sprintf("%d-%d-%" PRIx64, __pid,__tid,__i++);
+    static std::mutex __lock;
+    std::lock_guard<std::mutex> lg(__lock);
+    static size_t __i =0;
+    pid_t __pid = getpid();
+    pid_t __tid = (pid_t) syscall (SYS_gettid);
+    return cps_string::sprintf("%d-%d-%" PRIx64, __pid,__tid,__i++);
 }
 
 
