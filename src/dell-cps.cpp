dae56820fcbb478e42dfe489be160ecae19d2152
Cliff Wichmann <wichmann.ca@gmail.com>
2016-06-27 11:26:45 -0700

    Adding missing license.

--- src/dell-cps.cpp
+++ src/dell-cps.cpp
@@ -1,6 +1,18 @@
 /*
-dell-cps
-*/
+ * Copyright (c) 2016 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *  LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ * FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ */
 #include "dell-cps.h"
 #include "cps_class_map.h"
 
@@ -37,8 +49,8 @@ static struct {
 static const size_t lst_len = sizeof(lst)/sizeof(*lst);
   t_std_error cps_api_yang_module_init(void) {
     size_t ix = 0;
-    for ( ; ix < lst_len ; ++ix ) { 
-        cps_class_map_init(lst[ix].id,&(lst[ix]._ids[0]),lst[ix]._ids.size(),&lst[ix].details); 
+    for ( ; ix < lst_len ; ++ix ) {
+        cps_class_map_init(lst[ix].id,&(lst[ix]._ids[0]),lst[ix]._ids.size(),&lst[ix].details);
     }
     return STD_ERR_OK;
   }
