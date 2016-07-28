ef3c5677bb3ab51b1e525a02fc8c8783d959242e
Viraj Raiyani <viraj_raiyani@dell.com>
2016-07-28 14:09:14 -0700

    Segregating cps service and db/tunnel service

---
+++ scripts/bin/cps_api_db_service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2015 Dell Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+# LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+# FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+
+export PYTHONPATH=/opt/dell/os10/lib:/opt/dell/os10/lib/python
+/usr/bin/python /opt/dell/os10/lib/cps_stunnel_instance_manager.py & /usr/bin/python /opt/dell/os10/lib/cps_db_instance_manager.py
